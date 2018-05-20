d91369fa1ac4836a7850a66716871dc903582f09
Adam Kariv <adam.kariv@gmail.com>
2018-05-20 17:10:59 +0300

    0.2.11 Cleanup + fix dangling underline

--- src/components/BudgetKeyTooltipDirective.ts
+++ src/components/BudgetKeyTooltipDirective.ts
@@ -12,24 +12,9 @@ export class BudgetKeyTooltipDirective {
     constructor(el: ElementRef) {
         this.tooltipEl = el.nativeElement;
         el.nativeElement.classList.add('bk-tooltip-anchor');
-        // this.tooltipEl = document.createElement('p');        
-        // this.tooltipEl.className = 'bk-tooltip';
-        // el.nativeElement.after(this.tooltipEl);
     }
 
     ngOnInit() {
-        this.tooltipEl.innerHTML += `
-            <span class='bk-tooltip'>` + this.content + `</span>
-        `;
-        // console.log('ngOnInit!', this.content, );
-        // this.tooltipEl.innerHTML = this.content;
+        this.tooltipEl.innerHTML += `<span class='bk-tooltip'>` + this.content + `</span>`;
     }
-
-    // @HostListener('mouseenter') onMouseEnter() {
-    //     this.tooltipEl.classList.add('shown');
-    //   }
-      
-    //   @HostListener('mouseleave') onMouseLeave() {
-    //     this.tooltipEl.classList.remove('shown');
-    //   }
 }
